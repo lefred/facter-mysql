af198ffd496b88d2ac86e3738b46a5ffe1ffb434
Jan Vansteenkiste <jan@vstone.eu>
2012-11-16 09:58:48 +0100

    Check for the mysql command first.
    Without this, there are a lot of mysql command not found warnings which are annoying :)

--- mysql.rb
+++ mysql.rb
@@ -10,42 +10,46 @@
 
 # this file should be installed in your facter directory, for example on a fedora 14 it is
 # in /usr/lib/ruby/site_ruby/1.8/facter
- 
-# mysql command line to execute queries
-mysqlcmd = 'mysql -B -N -e'
-#status = %x[#{mysqlcmd} "SHOW STATUS"].to_s.strip
-status = %x[#{mysqlcmd} "SHOW STATUS"].split("\n")
-replica = %x[#{mysqlcmd} "SHOW SLAVE STATUS\\G"].split("\n")
+
+mysql_path = '/usr/bin/mysql'
+
+if File.exists?(mysql_path)
+
+  # mysql command line to execute queries
+  mysqlcmd = "#{mysql_path} -B -N -e"
+  #status = %x[#{mysqlcmd} "SHOW STATUS"].to_s.strip
+  status = %x[#{mysqlcmd} "SHOW STATUS"].split("\n")
+  replica = %x[#{mysqlcmd} "SHOW SLAVE STATUS\\G"].split("\n")
 
 
-Facter.add(:mysql_version) do
-  setcode do
-    isinstalled = false
-    os = Facter.value('operatingsystem')
-    case os
-      when "RedHat", "CentOS", "SuSE", "Fedora"
-        [ 'mysql-server', 'MySQL-server-percona', 'Percona-Server-server' ].each { |dbtype|
-          if isinstalled then next
-          isinstalled = system "rpm -qa #{dbtype}\* >/dev/null 2>&1"
-          end
-        }
-      when "Debian", "Ubuntu"
-        ['mysql-server', 'Percona-server'].each { |dbtype|
-          if isinstalled then next
-          isinstalled = system "dpkg -l #{dbtype} 2>&1 | egrep '(^ii|^hi)' >/dev/null"
-          end
-        }
-      else
-    end
-    if isinstalled then
-      %x[#{mysqlcmd} "SELECT VERSION()"].to_s.strip
+  Facter.add(:mysql_version) do
+    setcode do
+      isinstalled = false
+      os = Facter.value('operatingsystem')
+      case os
+        when "RedHat", "CentOS", "SuSE", "Fedora"
+          [ 'mysql-server', 'MySQL-server-percona', 'Percona-Server-server' ].each { |dbtype|
+            if isinstalled then next
+            isinstalled = system "rpm -qa #{dbtype}\* >/dev/null 2>&1"
+            end
+          }
+        when "Debian", "Ubuntu"
+          ['mysql-server', 'Percona-server'].each { |dbtype|
+            if isinstalled then next
+            isinstalled = system "dpkg -l #{dbtype} 2>&1 | egrep '(^ii|^hi)' >/dev/null"
+            end
+          }
+        else
+      end
+      if isinstalled then
+        %x[#{mysqlcmd} "SELECT VERSION()"].to_s.strip
+      end
     end
   end
-end
 
-mysqlversion = Facter.value('mysql_version')
+  mysqlversion = Facter.value('mysql_version')
 
-if mysqlversion then
+  if mysqlversion then
     Facter.add(:mysql_version_server) do
         setcode do
             %x[#{mysqlcmd} "SELECT @@VERSION_COMMENT"].to_s.strip
@@ -67,4 +71,5 @@ if mysqlversion then
         end
       end
     end
+  end
 end
